dd1879216c78283d4de4234d971ede25f4e375b4
yiminchen1976 <50095213+yiminchen1976@users.noreply.github.com>
2019-05-03 15:39:39 +0800

    Update HelloWorld.java

--- sample/pipeline-as-code/src/main/java/com/systex/HelloWorld.java
+++ sample/pipeline-as-code/src/main/java/com/systex/HelloWorld.java
@@ -2,6 +2,6 @@ package com.systex;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World! Hello World!");
+        System.out.println("Hello World! Hello World! XXXXXXXXXXXXXXXXXX");
     }
 }
